6a0326ab12ebf04fce1521b53e427b111726e6a5
DeepSeek67 <tolatan876@gwshare.com>
2026-08-28 18:35:40 +0800

    Add main plugin lifecycle

---
+++ src/main/java/com/cheatneutraliser/CheatNeutraliser.java
@@ -0,0 +1,30 @@
+package com.cheatneutraliser;
+
+import com.cheatneutraliser.analysis.AsyncAnalyzer;
+import com.cheatneutraliser.packet.PacketGuard;
+import com.cheatneutraliser.command.CheatNeutraliserCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CheatNeutraliser extends JavaPlugin {
+    private PacketGuard packetGuard;
+    private AsyncAnalyzer analyzer;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        analyzer = new AsyncAnalyzer(this);
+        packetGuard = new PacketGuard(this, analyzer);
+        packetGuard.start();
+        getCommand("cheatneutraliser").setExecutor(new CheatNeutraliserCommand(this));
+        getLogger().info("CheatNeutraliser enabled: packet safety + asynchronous neutralisation active.");
+    }
+
+    @Override
+    public void onDisable() {
+        if (packetGuard != null) packetGuard.stop();
+        if (analyzer != null) analyzer.shutdown();
+    }
+
+    public PacketGuard getPacketGuard() { return packetGuard; }
+    public AsyncAnalyzer getAnalyzer() { return analyzer; }
+}
